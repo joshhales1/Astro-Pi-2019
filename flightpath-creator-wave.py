473560a3ff24fb0c8939d81a3b020aa575969a2d
Josh Hales <joshhales2004@gmail.com>
2020-06-26 16:09:04 +0100

    Comments

--- flightpath-creator-wave.py
+++ flightpath-creator-wave.py
@@ -21,16 +21,15 @@ import json
 import os
 
 from math import degrees, atan2
-SCALE = 0.1
+SCALE = 0.1 # Can scale up and down the resolution of the ouptut. It needs to be slow so as to not fill memory
 
-ppd = 512.71209
-
+ppd = 512.71209 # Constant pixels per degree. See distance-reference folder for information
 
 data_file = open("results.json")
 data = json.loads(data_file.read()) # Load JSON file into program.
 data_file.close()
 
-boundries = {
+boundries = { # Object for holding boundry data for the canvas
 "long max": float("-inf"),
 "long min": float("inf"),
 "lat max": float("-inf"),
@@ -48,59 +47,60 @@ for i in range(1, len(data)): # Finds the upper and lower bounds of the long and
 x_length = (boundries["long max"] + abs(boundries["long min"])) * ppd * SCALE
 y_length = (boundries["lat max"] + abs(boundries["lat min"])) * ppd * SCALE
 
-x_length += (1920 * SCALE) // 2
+x_length += (1920 * SCALE) // 2 # Stops lower and upper images from rendering half off screen
 y_length += (1080 * SCALE) // 2
 
-background = Image.new("RGBA", (round(x_length), round(y_length)), (255, 255, 255))
 
 """
+Creates background image
 Correct scale causes memory errors.
 The image would be 184680px 51300px
 """
+background = Image.new("RGBA", (round(x_length), round(y_length)), (255, 255, 255)) #
 
-# The pixel per degree constant.
-old_long = 0
+
+old_long = 0 # For calculating theta between two frames
 old_lat  = 0
 
-start_image = 1
-end_image = len(data)
-step = 1
-percentage = 0
+start_image = 1 # The first image to render
+end_image = len(data) # The last image to render
+step = 1 # The step of each iteration
+percentage = 0 # For viewing progess in terminal
 for i in range(start_image, end_image, step):
 
-    im = Image.open("images/zz_oba_" + str(i) + ".jpg")
-    im = im.crop((400, 0, 1500, 1080))
+    im = Image.open("images/zz_oba_" + str(i) + ".jpg") # Open current image
+    im = im.crop((400, 0, 1500, 1080)) # Crop the window out of the image
 
     new_width = round(im.size[0] * SCALE)
     new_height = round(im.size[1] * SCALE)
 
-    im = im.resize((new_width, new_height), Image.ANTIALIAS)
+    im = im.resize((new_width, new_height), Image.ANTIALIAS) # Scales image down
 
-    x_offset = im.size[0] // 2
+    x_offset = im.size[0] // 2 # Calculating offset for later use to paste image by their center points
     y_offset = im.size[1] // 2
 
     raw_long = data[str(i)]["long"]
     raw_lat = data[str(i)]["lat"]
 
-    long = round(raw_long * ppd * SCALE)
+    long = round(raw_long * ppd * SCALE) # Get image friendly long and lats
     lat = round(raw_lat * ppd * SCALE)
 
-    long_distance = raw_long - old_long
+    long_distance = raw_long - old_long # Find difference between current and last image
     lat_distance  = raw_lat - old_lat
 
-    angle = atan2(lat_distance, long_distance)
-    angle = -degrees(angle)
+    angle = atan2(lat_distance, long_distance) # Calculate from the X axis in radians
+    angle = -degrees(angle) # Convert to degrees. Additionally, PIL uses angles anticlockwise so the negative value is required.
 
-    if angle < 0: angle = 360 + angle
+    if angle < 0: angle = 360 + angle # PIL also can't take negative values
 
-    im = im.convert("RGBA")
+    im = im.convert("RGBA") # Get an alpha channel to stop issues when rotating image
     im = im.rotate(angle, Image.NEAREST, expand = 1)
 
-    background.paste(im, (int(x_length // 2 + long) - x_offset, int(y_length // 2 + lat) - y_offset), im)
+    background.paste(im, (int(x_length // 2 + long) - x_offset, int(y_length // 2 + lat) - y_offset), im) # Paste image onto main canvas
     old_long = raw_long
     old_lat  = raw_lat
 
-    if percentage != int(i / end_image * 100):
+    if percentage != int(i / end_image * 100): # Just for visual benefit to see how long it will take
         print(str(percentage) + "%")
         percentage = int(i / end_image * 100)
 
